36e3e36f58c5d535625334facb8ac95c1bb4636f
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2022-09-01 22:42:50 +0300

    Update start.py

--- JE313P/plugins/start.py
+++ JE313P/plugins/start.py
@@ -5,15 +5,12 @@ from telethon import events, Button
 PM_START_TEXT = """
  👋┇بەخێر بێیت ئازیزم ! {}
 ➖➖➖➖➖➖➖➖➖➖➖➖➖➖
-✘من بۆتێکی سادەم بۆ پاراستنی گروپەکەت و پەخشکردنی کلیپی دەنگی لە پەیوەندییەکدا*.
+‣ **من بۆتێکی سادەم بۆ پاراستنی گروپەکەت و پەخشکردنی کلیپی دەنگی لە پەیوەندییەکدا**.
 ‣ **دەتوانم کلیپی دەنگی لە کاتی پەیوەندیدا لێبدەم**.
 ‣ **دەتوانم هەر بەکارهێنەرێک بلۆک بکەم و بێدەنگ بکەم**.
 ‣ **باشترین بۆت لە ڕووی تایبەتمەندییەوە**.
 ‣ **لەسەر بنەمای کتێبخانەی Telethon دروستکراوە بۆیە بۆتەکە خێرایە**!
 ➖➖➖➖➖➖➖➖➖➖➖➖➖➖
-✘ اضغط على الاسفل لعرض الاوامر الخاصه بي.
-[𖠄 𝗝𝗲𝗽𝘁𝗵𝗼𝗻 𝗨𝘀𝗲𝗿𝗯𝗼𝘁 𖠄](https://t.me/JEPTHON)
-"""
 
 @JE313P.on(events.NewMessage(pattern="^[?!/]start$"))
 async def start(event):
@@ -23,14 +20,14 @@ async def start(event):
              Config.START_IMG,
              caption=PM_START_TEXT.format(event.sender.first_name), 
              buttons=[
-        [Button.url("➕ اضغط هنا لأضافتي", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-        [Button.url("السورس", "https://t.me/JEPTHON")],
-        [Button.url("الدعم", f"https://t.me/{Config.SUPPORT}"), Button.url("القناة", f"https://t.me/{Config.CHANNEL}")],
-        [Button.inline("الاوامر", data="help")]])
+        [Button.url("➕ بۆ ئادم کلیک لێرە بکە", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+        [Button.url("خاوه ن بۆت", "https://t.me/SARKAUT")],
+        [Button.url("پشتگیری", f"https://t.me/{Config.SUPPORT}"), Button.url("که ناڵ", f"https://t.me/{Config.CHANNEL}")],
+        [Button.inline("فه رمانه کان", data="help")]])
        return
 
     if event.is_group:
-       await event.reply("**- اهلا بك انا اعمل بنجاح**")
+       await event.reply("**- من بە سەرکەوتوویی کار دەکەم**")
        return
 
 
@@ -39,8 +36,8 @@ async def start(event):
 async def _(event):
     if event.is_private:
        await event.edit(PM_START_TEXT.format(event.sender.first_name), buttons=[
-        [Button.url("➕ اضغط هنا لاضافتي", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-        [Button.url("السورس", "https://t.me/JEPTHON")],
-        [Button.url("الدعم", f"https://t.me/{Config.SUPPORT}"), Button.url("القناة", f"https://t.me/{Config.CHANNEL}")],
-        [Button.inline("الاوامر", data="help")]])
+        [Button.url("➕ بۆ ئادم کلیک لێرە بکە", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+        [Button.url("خاوه ن بۆت", "https://t.me/SARKAUT")],
+        [Button.url("پشتگتری", f"https://t.me/{Config.SUPPORT}"), Button.url("القناة", f"https://t.me/{Config.CHANNEL}")],
+        [Button.inline("فه رمانه کان", data="help")]])
        return
