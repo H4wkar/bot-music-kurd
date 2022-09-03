6b497b7a8dd94b1604e65a173d520acfb20e9207
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2022-09-03 03:19:57 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -9,5 +9,5 @@ class Config(object):
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
     SUPPORT = os.environ.get("SUPPORT", "JEPTHONSUPPORT")
     CHANNEL = os.environ.get("CHANNEL", "JEPTHON")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/5b2a6facf6f52aaa0ba23.jpg")
-    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/5b2a6facf6f52aaa0ba23.jpg")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/36caa6622e17edd915424.jpg")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/de449a6123872fed3eb5f.jpg")
